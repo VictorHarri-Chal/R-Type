f5248bbc5a3fcfe8db50b16529ff8e026b1595e2
matthis <matthis.lesur@epitech.eu>
2022-09-19 11:00:44 +0200

    Setup cmake

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** main
+*/
+
+#include <iostream>
+
+/**
+ * \file main.c
+ * \brief Programme de tests.
+ * \author Matthou
+ * \version 0.1
+ * \date 19 septembre 2022
+ *
+ * Programme de test
+ *
+ */
+
+/**
+ * \fn int main (void)
+ * \brief Entrée du programme.
+ *
+ * \return EXIT_SUCCESS - Arrêt normal du programme.
+ */
+
+int main()
+{
+    std::cout << "L'univers est un espace infini où s'entrechoque tous les hasards." << std::endl;
+    return 0;
+}
