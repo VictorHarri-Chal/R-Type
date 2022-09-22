f5340b88bebf8f34dc7051897d400ee8207f54ea
matthis <matthis.lesur@epitech.eu>
2022-09-22 14:50:37 +0200

    [FIX] clean main

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,6 @@
 
 #include <iostream>
 
-/**
- * \file main.c
- * \brief Programme de tests.
- * \author Matthou
- * \version 0.1
- * \date 19 septembre 2022
- *
- * Programme de test
- *
- */
-
-/**
- * \fn int main (void)
- * \brief Entrée du programme.
- *
- * \return EXIT_SUCCESS - Arrêt normal du programme.
- */
-
 int main()
 {
     std::cout << "L'univers est un espace infini où s'entrechoque tous les hasards." << std::endl;
