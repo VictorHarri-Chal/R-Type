73e9f83f26ca9089aad7af8fd198dff59f2ea8be
Nathan LB <nathan.le-bigot@epitech.eu>
2022-10-03 16:03:13 +0200

    [ADD] tools.system.package_manager in install script

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-mkdir -p build; cd build && conan install .. --build=missing -s compiler.libcxx=libstdc++11 && cmake -DCMAKE_BUILD_TYPE=Debug .. && cmake --build .
+mkdir -p build
+cd build
+sudo conan install .. -c tools.system.package_manager:mode=install --build=missing -s compiler.libcxx=libstdc++11
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake --build .
