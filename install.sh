e83c0cac07b9f142bef9ea043b2abab5862445ea
Nathan LB <nathan.le-bigot@epitech.eu>
2022-10-05 13:26:48 +0200

    [ADD] if else uname condition

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir -p build
 cd build
-conan install .. --build=missing -s compiler.libcxx=libstdc++11
-cmake -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build .
+if [ "$(uname)" = "Linux" ]; then
+  echo "building on linux"
+  conan install .. --build=missing -s compiler.libcxx=libstdc++11
+else
+  echo "building on macos or msys"
+  conan install .. --build=missing
+fi
+ cmake -DCMAKE_BUILD_TYPE=Debug ..
+ cmake --build .
+ 
