f50fd4f4b87fed9cee0b05e6c0824bc080bf4141
simlf <simon.le-fourn@epitech.eu>
2022-10-05 15:16:18 +0200

    [ADD] Override specifier to functions when needed

--- src/client/screens/multiplayer/MultiplayerScreen.hpp
+++ src/client/screens/multiplayer/MultiplayerScreen.hpp
@@ -20,7 +20,7 @@ namespace rtype
     {
         /**
          * @brief Class for the Menu Screen
-         * 
+         *
          */
         class MultiplayerScreen : public IScreen {
           public:
@@ -33,47 +33,47 @@ namespace rtype
             };
             /**
              * @brief Construct a new Menu Screen object
-             * 
+             *
              */
             MultiplayerScreen();
             /**
              * @brief Destroy the Menu Screen object
-             * 
+             *
              */
             ~MultiplayerScreen() = default;
             /**
              * @brief Init the Menu Screen
-             * 
+             *
              */
             void init() override;
             /**
              * @brief Draw the Menu Screen
-             * 
+             *
              */
             void draw(rtype::Game *gameEngine) override;
             /**
              * @brief Update the Menu Screen
-             * 
+             *
              */
             void update() override;
             /**
              * @brief Handle event for the Menu Screen
-             * 
+             *
              * @param event Event to be handled
              * @return int The Scene Number
              */
             int handleEvent(rtype::Event &event, rtype::Game *gameEngine) override;
-            
-            bool isButtonPressed(size_t index, rtype::Game *gameEngine, rtype::Event &event);
 
-            bool isMouseOnButton(size_t index, rtype::Game *gameEngine, rtype::Event &event);
+            bool isButtonPressed(size_t index, rtype::Game *gameEngine, rtype::Event &event) override;
+
+            bool isMouseOnButton(size_t index, rtype::Game *gameEngine, rtype::Event &event) override;
 
             bool isSurfaceClicked(float x, float y, float width, float height, rtype::Event &event, rtype::Game *gameEngine);
 
             bool isMouseOnSurface(float x, float y, float width, float height, rtype::Event &event, rtype::Game *gameEngine);
 
             int addRoom(float slot);
-            
+
             float checkForFreeSlot();
 
             void createRoom(rtype::Event &event, rtype::Game *gameEngine);
@@ -85,27 +85,27 @@ namespace rtype
           private:
             /**
              * @brief World object will contain all the entities and system
-             * 
+             *
              */
             rtype::ecs::world::World _world;
             /**
              * @brief Vector to store all rooms infos
-             * 
+             *
              */
             std::vector<room_t> _rooms;
             /**
              * @brief Vector to check if the mouse is on a button
-             * 
+             *
              */
             std::vector<bool> _buttons;
             /**
              * @brief Vector to check if rooms slots are empty or not
-             * 
+             *
              */
             std::vector<bool> _slots;
             /**
              * @brief Int who is being incremented everytime a room is created
-             * 
+             *
              */
             int _room_id;
 
