e1c70b412133e7d4261d649cff3bf62dbb86e0bd
Nathan LB <nathan.le-bigot@epitech.eu>
2022-10-07 11:15:52 +0200

    [ADD] script uname + arch struct

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+[ -f build ] && rm -fr build
 mkdir -p build
 cd build
+
 if [ "$(uname)" = "Linux" ]; then
-  echo "building on linux"
+  arch=$(dpkg --print-architecture)
+  if [ "$arch" = "amd64" ]; then
+    echo ubuntu
+  elif [ "$arch" = "arm64" ]; then
+    echo rpi
+  fi
   conan install .. --build=missing -s compiler.libcxx=libstdc++11
 else
-  echo "building on macos or msys"
   conan install .. --build=missing
 fi
- cmake -DCMAKE_BUILD_TYPE=Debug ..
- cmake --build .
- 
+  cmake -DCMAKE_BUILD_TYPE=Debug ..
+  cmake --build .
