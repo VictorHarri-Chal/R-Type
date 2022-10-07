1d5f903f207c29babf0c8386d7804e8648ac2d8f
Nathan LB <nathan.le-bigot@epitech.eu>
2022-10-07 11:31:02 +0200

    [FIX] typo and disable prompt

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ if [ "$(os_name)" = "Linux" ]; then
   arch=$(dpkg --print-architecture)
   sudo apt update
   sudo apt uprade -y
-  sudo apt install python3-pip
+  sudo apt install python3-pip -y
   sudo pip3 install conan
   if [ "$arch" = "amd64" ]; then
     echo ubuntu
@@ -18,7 +18,7 @@ if [ "$(os_name)" = "Linux" ]; then
     echo rpi
   fi
   conan install .. --build=missing -s compiler.libcxx=libstdc++11
-elif [ "$(os_name)" = "Linux" ]; then
+elif [ "$(os_name)" = "Darwin" ]; then
   echo macos
 else
   echo windows
