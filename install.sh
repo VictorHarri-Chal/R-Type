71e78faf67fd3e185350e7128c4a27a453ab52ce
Nathan LB <nathan.le-bigot@epitech.eu>
2022-10-11 13:50:38 +0200

    [FIX] Fedora: add some dependencies

--- install.sh
+++ install.sh
@@ -71,6 +71,42 @@ if [ "$os_name" = "Linux" ]; then
     sudo dnf install -y cmake
     sudo dnf install -y python3-pip
     sudo pip3 install conan
+    sudo dnf install -y \
+    libudev-dev \
+    libgl-dev \
+    libx11-xcb-dev \
+    libfontenc-dev \
+    libxaw7-dev \
+    libxcomposite-dev \
+    libxcursor-dev \
+    libxdamage-dev \
+    libxfixes-dev \
+    libxi-dev \
+    libxinerama-dev \
+    libxmu-dev \
+    libxmuu-dev \
+    libxpm-dev \
+    libxrandr-dev \
+    libxres-dev \
+    libxss-dev \
+    libxtst-dev \
+    libxv-dev \
+    libxvmc-dev \
+    libxxf86vm-dev \
+    libxcb-render-util0-dev \
+    libxcb-xkb-dev \
+    libxcb-icccm4-dev \
+    libxcb-image0-dev \
+    libxcb-keysyms1-dev \
+    libxcb-randr0-dev \
+    libxcb-shape0-dev \
+    libxcb-sync-dev \
+    libxcb-xfixes0-dev \
+    libxcb-xinerama0-dev \
+    libxcb-dri3-dev \
+    libxcb-util-dev \
+    libxcb-util0-dev \
+    systemd-devel
   fi
   conan install .. --build=missing -s compiler.libcxx=libstdc++11
 elif [ "$os_name" = "Darwin" ]; then
