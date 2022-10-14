62859b864a30b211b642e0a89c6da75111d5661d
Henri <henri.chauvet@epitech.eu>
2022-10-14 11:47:00 +0200

    [ADD] Full doc for screens in client

--- src/client/screens/IScreen.hpp
+++ src/client/screens/IScreen.hpp
@@ -37,12 +37,29 @@ namespace rtype
          * @brief Handle events
          * 
          * @param event Event to be handled
+         * @param gameEngine Game engine
          * @return int The scene number
          */
         virtual int handleEvent(rtype::Event &event, rtype::Game *gameEngine) = 0;
-
+        /**
+         * @brief Check if button is pressed
+         * 
+         * @param index Index of the button
+         * @param gameEngine Game engine
+         * @param event Event to be handled
+         * @return true If button is pressed
+         * @return false If button is not pressed
+         */
         virtual bool isButtonPressed(size_t index, rtype::Game *gameEngine, rtype::Event &event) = 0;
-
+        /**
+         * @brief Check if mouse is on button
+         * 
+         * @param index Index of the button
+         * @param gameEngine Game engine
+         * @param event Event to be handled
+         * @return true If mouse is on button
+         * @return false If mouse is not on button
+         */
         virtual bool isMouseOnButton(size_t index, rtype::Game *gameEngine, rtype::Event &event) = 0;
     };
 }
--- src/client/screens/intro/IntroScreen.hpp
+++ src/client/screens/intro/IntroScreen.hpp
@@ -41,27 +41,77 @@ namespace rtype
             void init() override;
             /**
              * @brief Update the Menu Screen
-             * 
+             * @param gameEngine Game engine
              */
             void update(rtype::Game *gameEngine) override;
             /**
              * @brief Handle event for the Menu Screen
              * 
              * @param event Event to be handled
+             * @param gameEngine Game engine
              * @return int The Scene Number
              */
             int handleEvent(rtype::Event &event, rtype::Game *gameEngine) override;
-            
+            /**
+            * @brief Check if button is pressed
+            * 
+            * @param index Index of the button
+            * @param gameEngine Game engine
+            * @param event Event to be handled
+            * @return true If button is pressed
+            * @return false If button is not pressed
+            */
             bool isButtonPressed(size_t index, rtype::Game *gameEngine, rtype::Event &event);
-
+            /**
+            * @brief Check if mouse is on button
+            * 
+            * @param index Index of the button
+            * @param gameEngine Game engine
+            * @param event Event to be handled
+            * @return true If mouse is on button
+            * @return false If mouse is not on button
+            */
             bool isMouseOnButton(size_t index, rtype::Game *gameEngine, rtype::Event &event);
-
+            /**
+             * @brief Check if surface is clicked
+             * 
+             * @param x X position of the surface
+             * @param y Y position of the surface
+             * @param width Width of the surface
+             * @param height Height of the surface
+             * @param event Event to be handled
+             * @param gameEngine Game engine
+             * @return true If surface is clicked
+             * @return false If surface is not clicked
+             */
             bool isSurfaceClicked(float x, float y, float width, float height, rtype::Event &event, rtype::Game *gameEngine);
-
+            /**
+             * @brief Check if surface is hovered
+             * 
+             * @param x X position of the surface
+             * @param y Y position of the surface
+             * @param width Width of the surface
+             * @param height Height of the surface
+             * @param event Event to be handled
+             * @param gameEngine Game engine
+             * @return true If surface is hovered
+             * @return false If surface is not hovered
+             */
             bool isMouseOnSurface(float x, float y, float width, float height, rtype::Event &event, rtype::Game *gameEngine);
-
+            /**
+             * @brief Check if hoover on button
+             * 
+             * @param event Event to be handled
+             * @param gameEngine Game engine
+             */
             void hooverOnButton(rtype::Event &event, rtype::Game *gameEngine);
-
+            /**
+             * @brief Check if isAlpha
+             * 
+             * @param event Event to be handled
+             * @return true If isAlpha
+             * @return false If is not Alpha
+             */
             bool isAlpha(rtype::Event &event);
 
           private:
@@ -75,9 +125,15 @@ namespace rtype
              * 
              */
             std::vector<bool> _buttons;
-
+            /**
+             * @brief String for the name of the player
+             * 
+             */
             std::string _pseudo;
-
+            /**
+             * @brief Map to check if on writing frame
+             * 
+             */
             bool _onWritingFrame;
         };
     }
