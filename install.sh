6de3248e8804d636c25ee72095624c6479e3a915
Henri <henri.chauvet@epitech.eu>
2022-10-18 11:11:49 +0200

    [REM] Removing manual installation of packages

--- install.sh
+++ install.sh
@@ -18,41 +18,6 @@ if [ "$os_name" = "Linux" ]; then
       sudo apt upgrade -y
       sudo apt install python3-pip -y
       sudo pip3 install conan
-      sudo apt install -y \
-      libudev-dev \
-      libgl-dev \
-      libx11-xcb-dev \
-      libfontenc-dev \
-      libxaw7-dev \
-      libxcomposite-dev \
-      libxcursor-dev \
-      libxdamage-dev \
-      libxfixes-dev \
-      libxi-dev \
-      libxinerama-dev \
-      libxmu-dev \
-      libxmuu-dev \
-      libxpm-dev \
-      libxrandr-dev \
-      libxres-dev \
-      libxss-dev \
-      libxtst-dev \
-      libxv-dev \
-      libxvmc-dev \
-      libxxf86vm-dev \
-      libxcb-render-util0-dev \
-      libxcb-xkb-dev \
-      libxcb-icccm4-dev \
-      libxcb-image0-dev \
-      libxcb-keysyms1-dev \
-      libxcb-randr0-dev \
-      libxcb-shape0-dev \
-      libxcb-sync-dev \
-      libxcb-xfixes0-dev \
-      libxcb-xinerama0-dev \
-      libxcb-dri3-dev \
-      libxcb-util-dev \
-      libxcb-util0-dev
       sudo apt install -f
       if [ "$arch" = "arm64" ]; then
           sudo apt install -y \
@@ -66,35 +31,15 @@ if [ "$os_name" = "Linux" ]; then
       sudo dnf install -y cmake
       sudo dnf install -y python3-pip
       sudo pip3 install conan
-      sudo dnf install -y \
-      libudev-devel \
-      libfontenc-devel \
-      libXaw-devel \
-      libXcomposite-devel \
-      libXdmcp-devel \
-      libXtst-devel \
-      libxkbfile-devel \
-      libXres-devel \
-      libXScrnSaver-devel \
-      libXvMC-devel \
-      xorg-x11-xtrans-devel \
-      xcb-util-wm-devel \
-      xcb-util-keysyms-devel \
-      xcb-util-renderutil-devel \
-      libXdamage-devel \
-      xcb-util-devel \
-      xkeyboard-config-devel \
-      systemd-devel \
-      libuuid-devel
     fi
   fi
   conan install .. --build=missing  -s compiler.libcxx=libstdc++11 -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
 elif [ "$os_name" = "Darwin" ]; then
   pip3 install conan
-  conan install .. --build=missing
+  conan install .. --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
 else
   pip3 install conan
-  conan install .. --build=missing
+  conan install .. --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
 fi
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 cmake --build .
