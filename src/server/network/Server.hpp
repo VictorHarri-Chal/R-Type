20473f32d26ae5692008e89c70702e6462c728c6
matthis <matthis.lesur@epitech.eu>
2022-10-23 22:46:53 +0200

    [UPD] add comment in Server.hpp

--- src/server/network/Server.hpp
+++ src/server/network/Server.hpp
@@ -27,23 +27,69 @@ using boost::asio::ip::udp;
  */
 class Server {
   public:
+    /**
+     * @brief Construct a new Server object
+     * 
+     * @param io_service 
+     * @param port 
+     */
     Server(boost::asio::io_service& io_service, int port) : _socket(io_service, udp::endpoint(udp::v4(), port)), _port(port), _roomId(0) {
       room_t room;
       room.id = -1;
       std::vector<room_t> tmp(7, room);
       _rooms = tmp;
       start_receive();
-  }
+    }
     void handle_send(const boost::system::error_code& /*error*/,
         std::size_t /*bytes_transferred*/);
-
+    /**
+     * @brief Send a message to client
+     * 
+     * @param type of the request
+     * @param value a int value if is needed
+     */
     void sendMessage(message::request type, int value = 0);
+    /**
+     * @brief Get the Buffer object
+     * 
+     * @return boost::array<char, 64> 
+     */
     boost::array<char, 64> getBuffer() const;
+    /**
+     * @brief Get the Room Id object
+     * 
+     * @return size_t 
+     */
     size_t getRoomId() const;
+    /**
+     * @brief Count nb room available
+     * 
+     * @return size_t 
+     */
     size_t countRoom();
+    /**
+     * @brief Set the Room Id object
+     * 
+     * @param roomId 
+     */
     void setRoomId(size_t roomId);
+    /**
+     * @brief Get the Rooms object
+     * 
+     * @return std::vector<room_t> 
+     */
     std::vector<room_t> getRooms() const;
+    /**
+     * @brief 
+     * 
+     * @param room 
+     */
     void addRooms(room_t room);
+    /**
+     * @brief Remove a room in the std::vector<room_t> _rooms
+     * 
+     * @param id 
+     */
     void removeRooms(int id);
   private:
     /**
@@ -86,21 +132,45 @@ class Server {
      * 
      */
     int _port;
+    /**
+     * @brief Message queue with all command receive
+     * 
+     */
     SafeQueue<message> _queue;
     /**
      * @brief Vector to store all rooms infos
      *
      */
     std::vector<room_t> _rooms;
+    /**
+     * @brief Count number of room created
+     * 
+     */
     size_t _roomId;
 };
-
+/**
+ * @brief HandleCommand class who execut command
+ * 
+ */
 class HandleCommand {
   private:
     std::vector<std::function<void(int, Server*)>> _allCommand;
 
   public:
+    /**
+     * @brief Construct a new Handle Command object
+     * 
+     */
     HandleCommand();
+    /**
+     * @brief Destroy the Handle Command object
+     * 
+     */
     ~HandleCommand() = default;
+    /**
+     * @brief Exectut the command ask
+     * 
+     * @param server 
+     */
     void findCmd(Server *server);
 };
