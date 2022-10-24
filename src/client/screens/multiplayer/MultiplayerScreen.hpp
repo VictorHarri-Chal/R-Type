bd36aa6ee0157e5d8380119c95b337c9146dd64b
matthis <matthis.lesur@epitech.eu>
2022-10-24 23:25:08 +0200

    [UPD] remove struct room in client

--- src/client/screens/multiplayer/MultiplayerScreen.hpp
+++ src/client/screens/multiplayer/MultiplayerScreen.hpp
@@ -26,43 +26,6 @@ namespace rtype
         class MultiplayerScreen : public IScreen {
           public:
             /**
-             * @brief Room struct
-             * 
-             * @param id Room id
-             * @param slot Room slot
-             * @param name Room name
-             * @param currPlayers Room current players
-             * @param isOpen Boolean if room is open
-             * 
-             */
-            struct room_t {
-                /**
-                 * @brief Room id
-                 * 
-                 */
-                int id;
-                /**
-                 * @brief Room slot
-                 * 
-                 */
-                int slot;
-                /**
-                 * @brief Room name
-                 * 
-                 */
-                std::string name;
-                /**
-                 * @brief Room current players
-                 * 
-                 */
-                unsigned int currPlayers;
-                /**
-                 * @brief Boolean if room is open
-                 * 
-                 */
-                bool isOpen;
-            };
-            /**
              * @brief Construct a new Multiplayer Screen object
              *
              */
