3402299241f36782aee01c821cc7a6ae0cde40a8
Henri <henri.chauvet@epitech.eu>
2022-10-25 13:37:56 +0200

    [ADD] Try with multiple catch for all types of exceptions

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -6,6 +6,10 @@
 */
 
 #include "game/Game.hpp"
+#include "../exceptions/Exceptions.hpp"
+#include "../exceptions/EcsExceptions.hpp"
+#include "../exceptions/GameExceptions.hpp"
+#include "../exceptions/ScreensExceptions.hpp"
 
 int main()
 {
@@ -15,8 +19,14 @@ int main()
         Game.init();
         Game.run();
         Game.destroy();
+    } catch (EcsExceptions &e) {
+        std::cerr << "Ecs Exception -> " << e.what() << std::endl;
+    } catch (GameExceptions &e) {
+        std::cerr << "Game Exception -> " << e.what() << std::endl;
+    } catch (ScreensExceptions &e) {
+        std::cerr << "Screen Exception -> " << e.what() << std::endl;
     } catch (std::exception &e) {
-        std::cerr << "Error: " << e.what() << std::endl;
+        std::cerr << "Global Exception -> " << e.what() << std::endl;
     }
     return 0;
 }
