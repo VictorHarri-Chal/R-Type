5dc2a37141f267463208e0f6b58789f534e6f2a9
Henri <henri.chauvet@epitech.eu>
2022-10-25 13:44:09 +0200

    [ADD] Exceptions for IntroScreen

--- src/client/screens/intro/IntroScreen.cpp
+++ src/client/screens/intro/IntroScreen.cpp
@@ -8,6 +8,7 @@
 #include "IntroScreen.hpp"
 #include "../../../ecs/System/Draw2D/draw2d.hpp"
 #include "../../../ecs/System/Movement/movement.hpp"
+#include "../../../exceptions/ScreensExceptions.hpp"
 
 rtype::menu::IntroScreen::IntroScreen(): _onWritingFrame(false)
 {
@@ -17,57 +18,83 @@ rtype::menu::IntroScreen::IntroScreen(): _onWritingFrame(false)
 void rtype::menu::IntroScreen::init()
 {
     rtype::ecs::system::ISystem *draw2DSystemMenu = new rtype::ecs::system::Draw2DSystem();
+    if (draw2DSystemMenu == nullptr)
+        throw ScreensExceptions("IntroScreen: Error while creating Draw2DSystem");
     this->_world.addSystem(draw2DSystemMenu);
     rtype::ecs::system::ISystem *movementSystemMenu = new rtype::ecs::system::MovementSystem();
+    if (movementSystemMenu == nullptr)
+        throw ScreensExceptions("IntroScreen: Error while creating MovementSystem");
     this->_world.addSystem(movementSystemMenu);
 
     rtype::ecs::entity::Entity *bg = new rtype::ecs::entity::Entity(rtype::ecs::entity::STATIC_SPRITE);
+    if (bg == nullptr)
+        throw ScreensExceptions("IntroScreen: Error while creating Entity (1)");
     bg->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, bg_x, 0.0f, -0.5f, 0.0f);
     bg->addComponent<ecs::component::Collide>(rtype::ecs::component::COLLIDE);
     bg->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "assets/bg.png", false, sf::Vector2f(1.f, 1.f), 0);
     this->_world.addEntity(bg);
     rtype::ecs::entity::Entity *stars = new rtype::ecs::entity::Entity(rtype::ecs::entity::STATIC_SPRITE);
+    if (stars == nullptr)
+        throw ScreensExceptions("IntroScreen: Error while creating Entity (2)");
     stars->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, stars_x, 0.0f, -0.7f, 0.0f);
     stars->addComponent<ecs::component::Collide>(rtype::ecs::component::COLLIDE);
     stars->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "assets/bg2.png", false, sf::Vector2f(1.f, 1.f), 0);
     this->_world.addEntity(stars);
     rtype::ecs::entity::Entity *planets = new rtype::ecs::entity::Entity(rtype::ecs::entity::STATIC_SPRITE);
+    if (planets == nullptr)
+        throw ScreensExceptions("IntroScreen: Error while creating Entity (3)");
     planets->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, planets_x, 0.0f, -1.0f, 0.0f);
     planets->addComponent<ecs::component::Collide>(rtype::ecs::component::COLLIDE);
     planets->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "assets/bg3.png", false, sf::Vector2f(1.f, 1.f), 0);
     this->_world.addEntity(planets);
     rtype::ecs::entity::Entity *bigPlanet = new rtype::ecs::entity::Entity(rtype::ecs::entity::STATIC_SPRITE);
+    if (bigPlanet == nullptr)
+        throw ScreensExceptions("IntroScreen: Error while creating Entity (4)");
     bigPlanet->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, bigPlanet_x, 700.f, -1.2f, 0.0f);
     bigPlanet->addComponent<ecs::component::Collide>(rtype::ecs::component::COLLIDE);
     bigPlanet->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "assets/bg4.png", false, sf::Vector2f(3.f, 3.f), 0);
     this->_world.addEntity(bigPlanet);
 
     rtype::ecs::entity::Entity *back = new rtype::ecs::entity::Entity(rtype::ecs::entity::RECTANGLE);
+    if (back == nullptr)
+        throw ScreensExceptions("IntroScreen: Error while creating Entity (5)");
     back->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 500.f, 100.f, 0.0f, 0.0f);
     back->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, 900.f, 800.f, sf::Color::Black, true, 3.0f, sf::Color::Magenta);
     this->_world.addEntity(back);
     rtype::ecs::entity::Entity *butt_validate = new rtype::ecs::entity::Entity(rtype::ecs::entity::RECTANGLE);
+    if (butt_validate == nullptr)
+        throw ScreensExceptions("IntroScreen: Error while creating Entity (6)");
     butt_validate->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 840.f, 800.f, 0.0f, 0.0f);
     butt_validate->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, 200.f, 50.f, sf::Color::Black, true, 3.0f, sf::Color::Magenta);
     this->_world.addEntity(butt_validate);
     _buttons.push_back(false);
     rtype::ecs::entity::Entity *validate = new rtype::ecs::entity::Entity(rtype::ecs::entity::TEXT);
+    if (validate == nullptr)
+        throw ScreensExceptions("IntroScreen: Error while creating Entity (7)");
     validate->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 880.f, 810.f, 0.0f, 0.0f);
     validate->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "Validate", 30.f, sf::Color::Magenta, true);
     this->_world.addEntity(validate);
     rtype::ecs::entity::Entity *name_frame = new rtype::ecs::entity::Entity(rtype::ecs::entity::RECTANGLE);
+    if (name_frame == nullptr)
+        throw ScreensExceptions("IntroScreen: Error while creating Entity (8)");
     name_frame->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 600.f, 200.f, 0.0f, 0.0f);
     name_frame->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, 700.f, 60.f, sf::Color::Black, true, 3.0f, sf::Color::Cyan);
     this->_world.addEntity(name_frame);
     rtype::ecs::entity::Entity *pseudo = new rtype::ecs::entity::Entity(rtype::ecs::entity::TEXT);
+    if (pseudo == nullptr)
+        throw ScreensExceptions("IntroScreen: Error while creating Entity (9)");
     pseudo->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 610.f, 215.f, 0.0f, 0.0f);
     pseudo->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "", 30.f, sf::Color::White, true);
     this->_world.addEntity(pseudo);
     rtype::ecs::entity::Entity *choose = new rtype::ecs::entity::Entity(rtype::ecs::entity::TEXT);
+    if (choose == nullptr)
+        throw ScreensExceptions("IntroScreen: Error while creating Entity (10)");
     choose->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 770.f, 140.f, 0.0f, 0.0f);
     choose->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "Choose a nickname", 40.f, sf::Color::White, true);
     this->_world.addEntity(choose);
     rtype::ecs::entity::Entity *warning = new rtype::ecs::entity::Entity(rtype::ecs::entity::TEXT);
+    if (warning == nullptr)
+        throw ScreensExceptions("IntroScreen: Error while creating Entity (11)");
     warning->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 650.f, 280.f, 0.0f, 0.0f);
     warning->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "Your nickname need to have at least 3 letters", 30.f, sf::Color::Red, true);
     this->_world.addEntity(warning);
