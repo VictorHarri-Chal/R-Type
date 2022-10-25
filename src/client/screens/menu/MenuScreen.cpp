8370e304ca5255d1c506a7c2647f3be2e657c238
Henri <henri.chauvet@epitech.eu>
2022-10-25 13:45:28 +0200

    [ADD] Exceptions for MenuScreen

--- src/client/screens/menu/MenuScreen.cpp
+++ src/client/screens/menu/MenuScreen.cpp
@@ -8,6 +8,7 @@
 #include "MenuScreen.hpp"
 #include "../../../ecs/System/Draw2D/draw2d.hpp"
 #include "../../../ecs/System/Movement/movement.hpp"
+#include "../../../exceptions/ScreensExceptions.hpp"
 
 rtype::menu::MenuScreen::MenuScreen()
 {
@@ -16,77 +17,109 @@ rtype::menu::MenuScreen::MenuScreen()
 void rtype::menu::MenuScreen::init()
 {
     rtype::ecs::system::ISystem *draw2DSystemMenu = new rtype::ecs::system::Draw2DSystem();
+    if (draw2DSystemMenu == nullptr)
+        throw ScreensExceptions("MenuScreen: Error while creating Draw2DSystem");
     this->_world.addSystem(draw2DSystemMenu);
     rtype::ecs::system::ISystem *movementSystemMenu = new rtype::ecs::system::MovementSystem();
+    if (movementSystemMenu == nullptr)
+        throw ScreensExceptions("MenuScreen: Error while creating MovementSystem");
     this->_world.addSystem(movementSystemMenu);
 
     rtype::ecs::entity::Entity *bg = new rtype::ecs::entity::Entity(rtype::ecs::entity::STATIC_SPRITE);
+    if (bg == nullptr)
+        throw ScreensExceptions("MenuScreen: Error while creating Entity (1)");
     bg->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, bg_x, 0.0f, -0.5f, 0.0f);
     bg->addComponent<ecs::component::Collide>(rtype::ecs::component::COLLIDE);
     bg->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "assets/bg.png", false, sf::Vector2f(1.f, 1.f), 0);
     this->_world.addEntity(bg);
     rtype::ecs::entity::Entity *stars = new rtype::ecs::entity::Entity(rtype::ecs::entity::STATIC_SPRITE);
+    if (stars == nullptr)
+        throw ScreensExceptions("MenuScreen: Error while creating Entity (2)");
     stars->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, stars_x, 0.0f, -0.7f, 0.0f);
     stars->addComponent<ecs::component::Collide>(rtype::ecs::component::COLLIDE);
     stars->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "assets/bg2.png", false, sf::Vector2f(1.f, 1.f), 0);
     this->_world.addEntity(stars);
     rtype::ecs::entity::Entity *planets = new rtype::ecs::entity::Entity(rtype::ecs::entity::STATIC_SPRITE);
+    if (planets == nullptr)
+        throw ScreensExceptions("MenuScreen: Error while creating Entity (3)");
     planets->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, planets_x, 0.0f, -1.0f, 0.0f);
     planets->addComponent<ecs::component::Collide>(rtype::ecs::component::COLLIDE);
     planets->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "assets/bg3.png", false, sf::Vector2f(1.f, 1.f), 0);
     this->_world.addEntity(planets);
     rtype::ecs::entity::Entity *bigPlanet = new rtype::ecs::entity::Entity(rtype::ecs::entity::STATIC_SPRITE);
+    if (bigPlanet == nullptr)
+        throw ScreensExceptions("MenuScreen: Error while creating Entity (4)");
     bigPlanet->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, bigPlanet_x, 700.f, -1.2f, 0.0f);
     bigPlanet->addComponent<ecs::component::Collide>(rtype::ecs::component::COLLIDE);
     bigPlanet->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "assets/bg4.png", false, sf::Vector2f(3.f, 3.f), 0);
     this->_world.addEntity(bigPlanet);
 
     rtype::ecs::entity::Entity *butt_solo = new rtype::ecs::entity::Entity(rtype::ecs::entity::RECTANGLE);
+    if (butt_solo == nullptr)
+        throw ScreensExceptions("MenuScreen: Error while creating Entity (5)");
     butt_solo->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 870.f, 390.f, 0.0f, 0.0f);
     butt_solo->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, 70.f, 50.f, sf::Color::Transparent, true, 3.0f, sf::Color::Transparent);
     this->_world.addEntity(butt_solo);
     _buttons.push_back(false);
     rtype::ecs::entity::Entity *solo = new rtype::ecs::entity::Entity(rtype::ecs::entity::TEXT);
+    if (solo == nullptr)
+        throw ScreensExceptions("MenuScreen: Error while creating Entity (6)");
     solo->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 870.f, 400.f, 0.0f, 0.0f);
     solo->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "Solo", 30.f, sf::Color::Blue, true);
     this->_world.addEntity(solo);
 
     rtype::ecs::entity::Entity *butt_multi = new rtype::ecs::entity::Entity(rtype::ecs::entity::RECTANGLE);
+    if (butt_multi == nullptr)
+        throw ScreensExceptions("MenuScreen: Error while creating Entity (7)");
     butt_multi->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 870.f, 460.f, 0.0f, 0.0f);
     butt_multi->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, 170.f, 50.f, sf::Color::Transparent, true, 3.0f, sf::Color::Transparent);
     this->_world.addEntity(butt_multi);
     _buttons.push_back(false);
     rtype::ecs::entity::Entity *multi = new rtype::ecs::entity::Entity(rtype::ecs::entity::TEXT);
+    if (multi == nullptr)
+        throw ScreensExceptions("MenuScreen: Error while creating Entity (8)");
     multi->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 870.f, 470.f, 0.0f, 0.0f);
     multi->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "Multiplayer", 30.f, sf::Color::Blue, true);
     this->_world.addEntity(multi);
 
     rtype::ecs::entity::Entity *butt_htp = new rtype::ecs::entity::Entity(rtype::ecs::entity::RECTANGLE);
+    if (butt_htp == nullptr)
+        throw ScreensExceptions("MenuScreen: Error while creating Entity (9)");
     butt_htp->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 870.f, 530.f, 0.0f, 0.0f);
     butt_htp->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, 190.f, 50.f, sf::Color::Transparent, true, 3.0f, sf::Color::Transparent);
     this->_world.addEntity(butt_htp);
     _buttons.push_back(false);
     rtype::ecs::entity::Entity *htp = new rtype::ecs::entity::Entity(rtype::ecs::entity::TEXT);
+    if (htp == nullptr)
+        throw ScreensExceptions("MenuScreen: Error while creating Entity (10)");
     htp->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 870.f, 540.f, 0.0f, 0.0f);
     htp->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "How to play", 30.f, sf::Color::Blue, true);
     this->_world.addEntity(htp);
 
     rtype::ecs::entity::Entity *butt_options = new rtype::ecs::entity::Entity(rtype::ecs::entity::RECTANGLE);
+    if (butt_options == nullptr)
+        throw ScreensExceptions("MenuScreen: Error while creating Entity (11)");
     butt_options->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 870.f, 600.f, 0.0f, 0.0f);
     butt_options->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, 120.f, 50.f, sf::Color::Transparent, true, 3.0f, sf::Color::Transparent);
     this->_world.addEntity(butt_options);
     _buttons.push_back(false);
     rtype::ecs::entity::Entity *options = new rtype::ecs::entity::Entity(rtype::ecs::entity::TEXT);
+    if (options == nullptr)
+        throw ScreensExceptions("MenuScreen: Error while creating Entity (12)");
     options->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 870.f, 610.f, 0.0f, 0.0f);
     options->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "Options", 30.f, sf::Color::Blue, true);
     this->_world.addEntity(options);
 
     rtype::ecs::entity::Entity *butt_quit = new rtype::ecs::entity::Entity(rtype::ecs::entity::RECTANGLE);
+    if (butt_quit == nullptr)
+        throw ScreensExceptions("MenuScreen: Error while creating Entity (13)");
     butt_quit->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 870.f, 670.f, 0.0f, 0.0f);
     butt_quit->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, 70.f, 50.f, sf::Color::Transparent, true, 3.0f, sf::Color::Transparent);
     this->_world.addEntity(butt_quit);
     _buttons.push_back(false);
     rtype::ecs::entity::Entity *quit = new rtype::ecs::entity::Entity(rtype::ecs::entity::TEXT);
+    if (quit == nullptr)
+        throw ScreensExceptions("MenuScreen: Error while creating Entity (14)");
     quit->addComponent<ecs::component::Transform>(rtype::ecs::component::TRANSFORM, 870.f, 680.f, 0.0f, 0.0f);
     quit->addComponent<ecs::component::Drawable2D>(rtype::ecs::component::DRAWABLE2D, "Quit", 30.f, sf::Color::Blue, true);
     this->_world.addEntity(quit);
